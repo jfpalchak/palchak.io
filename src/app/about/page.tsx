0b4fd64b81e0d52a1190cf17f029382201b8ed68
Joey Palchak <joey.palchak@gmail.com>
2024-03-15 11:25:16 -0700

    create basic project structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'About',
+};
+
+export default function AboutPage() {
+  return (
+    <div>About</div>
+  )
+}
