219c3a698561ae2347c88c68c6b40c8bd496fd22
Joey Palchak <joey.palchak@gmail.com>
2024-03-15 22:59:58 -0700

    add svg icons for links & socials

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as ArrowLinkIcon } from '@/components/icons/ArrowLinkIcon';
+export { default as ChainLinkIcon } from '@/components/icons/ChainLinkIcon';
+export { default as GithubIcon } from '@/components/icons/GithubIcon';
+export { default as LinkedInIcon } from '@/components/icons/LinkedInIcon';
