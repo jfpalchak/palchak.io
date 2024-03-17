d73ba023f04f7026de6c94b05a3dba6e64ef2683
Joey Palchak <joey.palchak@gmail.com>
2024-03-16 19:17:07 -0700

    create Projects component + add project logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,16 @@
-import { Summary, MyProjects } from "@/data/lifeApi";
-import { ArrowLinkIcon } from "@/components/icons";
+import { SUMMARY } from "@/data/lifeApi";
 import Socials from "@/components/Socials";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <section>
-      {/* SUMMARY */}
       <div>
         <h1 className="text-3xl font-medium">Joey Palchak</h1>
-        <p className="text-sm sm:text-base mt-6">{Summary}</p>
+        <p className="text-sm sm:text-base mt-6">{SUMMARY}</p>
       </div>
-      {/* SOCIALS */}
       <Socials />
-      {/* PROJECTS */}
-      <div>
-        <h2 className="text-lg font-medium mt-8">Projects</h2>
-        {MyProjects.map(({ stack, links, ...project }) => (
-          <div key={project.name} className="sm:grid sm:grid-cols-4 mb-5 min-h-[250px]">
-            <p className="pt-3 pl-3 sm:pl-0 text-sm text-neutral-500">{project.status}</p>
-            <div className="flex flex-col sm:justify-between p-3 col-span-3 transition-all rounded-lg group hover:bg-neutral-100 hover:cursor-pointer">
-              <h3>{project.name}</h3>
-              <p className="text-sm text-neutral-500">{project.role}</p>
-              <p className="text-sm mt-3">{project.description}</p>
-              <ul className="flex flex-wrap gap-1 mt-5">
-                {stack.map((tech) => (
-                  <li key={tech} className="items-center whitespace-nowrap rounded-full bg-gray-600/10 py-1 px-3 text-xs text-gray-900">
-                    <span>{tech}</span>
-                  </li>
-                ))}
-              </ul>
-              <div className="flex justify-between mt-4 sm:mt-auto text-sm text-neutral-500">
-                {links.map(({ label, href }) => (
-                  <a key={label} href={href} className="group flex gap-1 transition group-hover:text-neutral-800 hover:underline">
-                    {label}
-                    <ArrowLinkIcon className="w-4 h-5 fill-neutral-500 transition group-hover:fill-neutral-800" />
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Projects />
     </section>
   );
 }
--- src/data/lifeApi.ts
+++ src/data/lifeApi.ts
@@ -1,17 +1,17 @@
-import { GithubIcon, LinkedInIcon } from "@/components/icons";
+import { GithubIcon, LinkedInIcon, AtIcon } from "@/components/icons";
 
-export const Summary = 'Full Stack Developer, collaborative story-teller, and a maker of things. Enjoys building creative, dynamic products from start to finish. Focused on delivering intuitive experiences with extra attention to detail.';
+export const SUMMARY = 'Full Stack Developer, collaborative story-teller, and a maker of things. Enjoys building creative, dynamic products from start to finish. Focused on delivering intuitive experiences with extra attention to detail.';
 
-export const About = [
-  `I'm Joey! I'm a developer based in Portland, Oregon, having grown up in the small town of Colchester, Vermont.`,
+export const ABOUT = [
+  `Hi, I'm Joey! I'm a developer based in Portland, Oregon, having grown up in the small town of Colchester, Vermont.`,
   `I have a bachelor's degree in Computer Science from the University of Vermont, and studied Full Stack Web Development at Epicodus, a vocational school in Portland.`,
   `I enjoy programming, dungeons & dragons, hiking with my dog, and every possible iteration of grapefruit-flavored seltzer.`,
 ];
 
-export const SocialMedia = [
+export const SOCIAL_MEDIA = [
   { name: 'Github', link: 'https://github.com/jfpalchak', icon: GithubIcon },
   { name: 'LinkedIn', link: 'https://www.linkedin.com/in/joey-palchak/', icon: LinkedInIcon },
-  // { name: 'Email', link: 'mailto:joey.palchak@gmail.com', icon: '' },
+  { name: 'Email', link: 'mailto://joey.palchak@gmail.com', icon: AtIcon },
 ] as const;
 
 export interface Project {
@@ -27,7 +27,7 @@ export interface Project {
   }[];
 };
 
-export const MyProjects: Project[] = [
+export const PROJECTS: Project[] = [
   {
     name: 'VinoSeeker',
     role: 'Full Stack Developer',
@@ -37,13 +37,14 @@ export const MyProjects: Project[] = [
       'React',
       'Redux',
       'Ionic',
+      'SASS',
       'Tailwind',
       'Node.js',
       'Express.js',
       'PostgreSQL',
     ],
     description: 'A geo-social networking service that allows users to discover wines and wineries, record their experiences, and share with friends.',
-    logo: '',
+    logo: '/logos/vinoseeker.png',
     links: [
       { label: 'vinoseeker.com', href: 'https://vinoseeker.com/' },
     ]
@@ -62,10 +63,10 @@ export const MyProjects: Project[] = [
       'MySQL',
     ],
     description: 'A play on "quitting cold turkey," Cranberry is a daily tracker with motivating metrics to help users quit smoking.',
-    logo: '',
+    logo: '/logos/cranberry.png',
     links: [
-      { label: 'github.com', href: 'https://github.com/jfpalchak/cranberry' },
       { label: 'cranberry.onrender.com', href: 'http://cranberry-axec.onrender.com/' },
+      { label: 'github.com', href: 'https://github.com/jfpalchak/cranberry' },
     ]
   },
   {
@@ -81,10 +82,10 @@ export const MyProjects: Project[] = [
       'MySQL',
     ],
     description: 'An early 2000\'s inspired public chat room, with user-to-user private chats.',
-    logo: '',
+    logo: '/logos/eol.svg',
     links: [
-      { label: 'github.com', href: 'https://github.com/jfpalchak/EOL.Solution' },
       { label: 'eol-app.onrender.com', href: 'https://eol-app.onrender.com/' },
+      { label: 'github.com', href: 'https://github.com/jfpalchak/EOL.Solution' },
     ]
   },
   {
@@ -99,10 +100,10 @@ export const MyProjects: Project[] = [
       'babel',
     ],
     description: 'A text-based adventure game, where the user navigates and interacts with the world via text input.',
-    logo: '',
+    logo: '/logos/adventure.svg',
     links: [
-      { label: 'github.com', href: 'https://github.com/jfpalchak/text-adventure' },
       { label: 'github.io/text-adventure', href: 'https://jfpalchak.github.io/text-adventure/' },
+      { label: 'github.com', href: 'https://github.com/jfpalchak/text-adventure' },
     ]
   },
 ]
