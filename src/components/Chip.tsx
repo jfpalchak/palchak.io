926c913d8aa9385157d568bccc3235e3e577b5ec
Joey Palchak <joey.palchak@gmail.com>
2024-03-17 22:28:50 -0700

    update Chip prop type

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,6 +1,8 @@
-import { PropsWithChildren } from 'react';
+interface Props {
+  children: React.ReactNode;
+};
 
-export default function Chip({ children }: PropsWithChildren) {
+export default function Chip({ children }: Props) {
   return (
     <button className="items-center whitespace-nowrap rounded-full bg-gray-600/10 px-3 py-1 text-xs text-gray-900">
       {children}
