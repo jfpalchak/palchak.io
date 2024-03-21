c4666a74b0ec6efeee2d2952acce7c39b1f8fcfe
Joey Palchak <joey.palchak@gmail.com>
2024-03-21 13:09:54 -0700

    update chip dark styles

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Chip({ children }: Props) {
   return (
-    <button className="items-center whitespace-nowrap rounded-full bg-neutral-600/10 px-3 py-1 text-xs text-neutral-900 dark:bg-neutral-500/10 dark:text-neutral-500">
+    <button className="items-center whitespace-nowrap rounded-full bg-neutral-600/10 px-3 py-1 text-xs text-neutral-900 dark:bg-neutral-400/10 dark:text-neutral-300">
       {children}
     </button>
   );
