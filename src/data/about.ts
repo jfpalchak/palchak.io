67e786a4100e6b6e23e64e6d14bb7cca05bd50f4
Joey Palchak <joey.palchak@gmail.com>
2024-03-22 12:53:51 -0700

    edit summary text

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,7 +1,7 @@
 
 export const NAME = 'Joey Palchak';
 
-export const SUMMARY = 'Full Stack Developer, collaborative story-teller, and a maker of things. Enjoys building creative, dynamic products from start to finish. Focused on delivering intuitive experiences with extra attention to detail.';
+export const SUMMARY = 'Full Stack Developer, collaborative story-teller, and a maker of things. Deeply curious and dedicated to continuous learning. Focused on building intuitive experiences with extra attention to detail.';
 
 export const ABOUT = [
   `Hi, I'm Joey! I'm a developer based in Portland, Oregon, having grown up in the small town of Colchester, Vermont.`,
