71c5c51355fce88ee32a269bb1231ee895d24b44
Joey Palchak <joey.palchak@gmail.com>
2024-04-05 00:37:02 -0700

    add aria-labels to social links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -6,7 +6,7 @@ export default function Socials() {
     <ul className="flex gap-4">
       {SOCIAL_MEDIA.map(({ name, link, icon: Icon }) => (
         <li key={name}>
-          <ExternalLink href={link}>
+          <ExternalLink href={link} aria-label={`Link to ${name}.`}>
             <Icon className="h-5 w-5 fill-neutral-400 transition hover:fill-neutral-800 dark:hover:fill-neutral-100 dark:fill-neutral-500" />
           </ExternalLink>
         </li>
